e25c08eb2ba36505651f5a4ee2baa2bff8082936
Colin Leahy <ccleahy00@hotmail.com>
2018-03-29 16:37:14 -0500

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+// https://serverjs.io/
+const server = require('server');
+const { get, socket } = server.router;
+const { render } = server.reply;
+
+// Update everyone with the current user count
+const updateCounter = ctx => {
+  ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
+};
+
+// Send the new message to everyone
+const sendMessage = ctx => {
+  ctx.io.emit('message', ctx.data);
+};
+
+const userJoin = ctx => {
+    ctx.io.emit('join', ctx.data)
+};
+
+server([
+  get('/', ctx => render('index.html')),
+  socket('connect', updateCounter),
+  socket('disconnect', updateCounter),
+  socket('message', sendMessage),
+  socket('join', userJoin)
+]);
